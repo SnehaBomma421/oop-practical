485b7d42ced1fa670d73021cad8d6e89091feb66
SnehaBomma421 <2420030231@klh.edu.in>
2025-08-09 10:32:44 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,18 @@
+package lab3;
+import java.util.Scanner;
+public class Palindrome {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter a string: ");
+	        String str = sc.nextLine();
+
+	        String reversed = new StringBuilder(str).reverse().toString();
+
+	        if (str.equals(reversed)) {
+	            System.out.println("Palindrome");
+	        } else {
+	            System.out.println("Not a palindrome");
+	        }
+	    }
+	}
+
